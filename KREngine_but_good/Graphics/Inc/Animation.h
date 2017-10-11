ecaa5e2ebafe2ef4db1847621b538742391ce1ad
Jake Roman <bowenrombar@gmail.com>
2017-10-11 15:23:16 -0700

    [Animation] - Implemented GetTransform - Implemented AddKeyrame/SortKeyframe

--- KREngine_but_good/Graphics/Inc/Animation.h
+++ KREngine_but_good/Graphics/Inc/Animation.h
@@ -5,28 +5,36 @@
 namespace Graphics
 {
 
-	struct Keyframe
+struct Keyframe
+{
+	float time;
+	Math::Vector3 position;
+	Math::Vector3 scale;
+	Math::Quaternion rotation;
+
+	bool operator < (const Keyframe& rhs)
 	{
-		float time;
-		Math::Vector3 position;
-		Math::Vector3 scale;
-		Math::Quaternion rotation;
-	};
+		return time < rhs.time;
+	}
+}; // struct Keyframe
 
-	class Animation
-	{
-	private:
-		std::vector<Keyframe> mKeyframes;
-		bool bLoop;
-	public:
-		Animation();
-		~Animation();
-		void Initialize(bool isLooped, uint32_t numKeyframes = 10);
-		void Terminate();
+class Animation
+{
+private:
+	std::vector<Keyframe> mKeyframes;
+	bool bLoop;
+public:
+	Animation();
+	~Animation();
+	void Initialize(bool isLooped, uint32_t numKeyframes = 10);
+	void Terminate();
+	// TODO: When implementing Play() ensure that the user has given a frame at time 0.0 
+	Math::Matrix4 GetTransform(float time) const;
 
-		Math::Matrix4 GetTransform(float time) const;
+	void SortKeyframes();
 
-		void AddKeyframe(Keyframe newFrame);
-		void AddKeyframe(float keyTime, Math::Vector3 keyPos, Math::Vector3 keyScale, Math::Quaternion keyRot);
-	};
-}
+	void AddKeyframe(Keyframe newFrame);
+	void AddKeyframe(float keyTime, Math::Vector3 keyPos, Math::Vector3 keyScale, Math::Quaternion keyRot);
+}; // class Animation
+
+} // namespace Graphics
--- KREngine_but_good/Graphics/Src/Animation.cpp
+++ KREngine_but_good/Graphics/Src/Animation.cpp
@@ -22,21 +22,65 @@ void Animation::Terminate()
 	mKeyframes.clear();
 }
 
-Math::Matrix4 Graphics::Animation::GetTransform(float time) const
+// Returns the transform matrix for a given time frame
+Math::Matrix4 Animation::GetTransform(float time) const
 {
+	ASSERT(time < mKeyframes.back().time, "[Animation] Given time exceeds keyframes.")
 	ASSERT(mKeyframes.size() == 0, "[Animation] Error rendering. No Keyframes.");
 
-	return Math::Matrix4::Identity();
+	int startFrameIdx = 0;
+	int endFrameIdx = 0;
+
+	// iterate through vector to find the two closest keyframes to the given time
+	for (; startFrameIdx < mKeyframes.size() - 1; ++startFrameIdx)
+	{
+		if (mKeyframes[startFrameIdx].time <= time && mKeyframes[startFrameIdx + 1].time >= time)
+		{
+			endFrameIdx = startFrameIdx + 1;
+			break;
+		}
+	}
+
+	// construct a transform based on the given time's interpolant between the two keyframes
+	const Keyframe& startFrame = mKeyframes[startFrameIdx];
+	const Keyframe& endFrame = mKeyframes[endFrameIdx];
+	float interpolant = (time - startFrame.time) / (endFrame.time - startFrame.time);
+
+	Math::Vector3 PosAtTime = Math::Lerp(startFrame.position, endFrame.position, interpolant);
+	Math::Vector3 ScaleAtTime = Math::Lerp(startFrame.scale, endFrame.scale, interpolant);
+	Math::Quaternion RotAtTime = Math::Slerp(startFrame.rotation, endFrame.rotation, interpolant);
+
+	Math::Matrix4 scaleMat = Math::Matrix4::Scaling(ScaleAtTime);
+	Math::Matrix4 rotMat = Math::Matrix4::RotationQuaternion(RotAtTime);
+	Math::Matrix4 transMat = Math::Matrix4::Translation(PosAtTime);
+	return scaleMat * rotMat * transMat;
 }
 
+// Sorts all Keyframes within mKeyframes based on ascending time variables
+void Animation::SortKeyframes()
+{
+	// iterate through vector and sort based on time
+	std::sort(mKeyframes.begin(), mKeyframes.end());
+}
+
+// Adds the given Keyframe to the mKeyframes vector
 void Animation::AddKeyframe(Keyframe newFrame)
 {
+	// search the vector to make sure a frame does not already exist with the same time as newFrame
+	if (std::find(mKeyframes.begin(), mKeyframes.end(), [&newFrame](const Keyframe& val)
+	{
+		return val.time == newFrame.time;
+	}) != mKeyframes.end())
+	{
+		LOG("[Animation] Error: Frame already exists with given time.");
+		return;
+	}
 	mKeyframes.push_back(newFrame);
-
-	// iterate through vector and sort based on time
+	SortKeyframes();
 }
 
-void Graphics::Animation::AddKeyframe(float keyTime, Math::Vector3 keyPos, Math::Vector3 keyScale, Math::Quaternion keyRot)
+// Constructs a Keyframe from the given values and inserts it into the mKeyframes vector
+void Animation::AddKeyframe(float keyTime, Math::Vector3 keyPos, Math::Vector3 keyScale, Math::Quaternion keyRot)
 {
-	mKeyframes.push_back(Keyframe{ keyTime,keyPos,keyScale,keyRot });
+	AddKeyframe(Keyframe{ keyTime,keyPos,keyScale,keyRot });
 }
